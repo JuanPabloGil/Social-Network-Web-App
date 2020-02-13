70221b9fda030e338cf9ccb7fd0fd73159a89e9f
Juan Pablo <jpablomgil@gmail.com>
2020-02-13 13:48:31 -0600

    Create route to create and destroy friendship

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show] do
-    resources :friendships, only: [:create, :destroy]
+    resources :friendships, only: [:create, :destroy ]
   end
 
   resources :posts, only: [:index, :create] do
