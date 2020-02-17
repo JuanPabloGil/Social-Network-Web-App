d4ec0dede23a978197773a77b41f794634428b56
Juan Pablo <jpablomgil@gmail.com>
2020-02-17 08:37:18 -0600

    Validate email of user with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 
   has_many :posts
   has_many :comments, dependent: :destroy
